7bfbd5eff0565331a5caf2bc67cacf0796e3916f
LuisReagalado <luis.regalado@cceo.com.mx>
2020-02-13 11:24:05 -0600

    i just finished de part 6 of vue course. I changed components to add homewor and list of them from html to js

--- 4.1/vue.js
+++ 4.1/vue.js
@@ -1,7 +1,5 @@
-var app = new Vue ({
-	el: '#app',
-	data:{
-		titulo: 'lista de tareas',
+
+var	data = {
 		tareas: [
 			{
 				texto: 'aprende viu.js',
@@ -17,20 +15,61 @@ var app = new Vue ({
 			},
 		],
 		nuevaTarea: ''
-	},
-	methods:{
-		agregartarea: function(){
-			var texto = this.nuevaTarea.trim();
-			if(texto){
-				this.tareas.push({
-					texto:texto,
-					terminada: false
-				})
-			}
-			this.nuevaTarea='';
+	}
+Vue.component('titulo',{
+	template: '<h2> {{titulo}}</h2>',
+	data: function (){
+		return {titulo: '--lista de tareas-- '}
+	}
+})
+Vue.component ('nueva-tarea',{
+	template: `
+		<div class="input-group">
+			<input type="text" placeholder="Nueva tarea" v-model="nuevaTarea" class="form-control" @keyup.enter="agregartarea">
+			<span class="input-group-btn">
+				<button type="button" @click="agregartarea" class="btn btn-primary">Agregar</button>
+			</span>
+		</div>`,
+		data: function (){
+			return data;
 		},
+		methods: {
+			agregartarea: function(){
+				var texto = this.nuevaTarea.trim();
+				if(texto){
+					this.tareas.push({
+						texto:texto,
+						terminada: false
+					})
+				}
+				this.nuevaTarea='';
+			}
+		}
+})
+Vue.component ('lista-de-tareas',{
+	template:`
+		<ul class="list-group">
+			<li v-for="(tarea, indice) of tareas" class="list-group-item" v-bind:class="{terminada: tarea.terminada}">
+				{{tarea.texto}}
+				<span class="pull-right">
+					<button type="button" class="btn btn-success btn-xs glyphicon glyphicon-ok" @click="tarea.terminada=true"></button>
+					<button type="button" class="btn btn-danger btn-xs glyphicon glyphicon-remove" @click="borrar(indice)"></button>
+				</span>
+			</li>
+		</ul>
+	`,
+	data: function(){
+		return data;
+	},
+	methods: {
 		borrar: function(indice){
 			this.tareas.splice(indice,1);
 		}
+	}
+})
+var app = new Vue ({
+	el: '#app',
+	data: data,
+	methods:{
 	}
 })
